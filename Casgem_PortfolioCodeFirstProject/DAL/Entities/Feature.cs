6b91f61ef4df2cba7d9234d6eb9b381dfaaaa2f9
oguzhan2027 <oguzhancelik2027@gmail.com>
2023-07-16 14:25:16 +0300

    entities

---
+++ Casgem_PortfolioCodeFirstProject/DAL/Entities/Feature.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Casgem_PortfolioCodeFirstProject.DAL.Entities
+{
+    public class Feature
+    {
+    }
+}
